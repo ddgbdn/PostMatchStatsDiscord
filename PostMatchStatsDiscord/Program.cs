3526ab2075eb10f4cbfd1cb0814a46a0abfffa53
B <bogdan13531@gmail.com>
2022-10-09 23:21:19 +0600

    Add project files.

---
+++ PostMatchStatsDiscord/Program.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    public static Task Main(string[] args)
+        => new Program().MainAsync();
+
+    private DiscordSocketClient _client;
+
+    public async Task MainAsync()
+    { 
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        var token = Environment.GetEnvironmentVariable("StratzDiscordBotToken");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
