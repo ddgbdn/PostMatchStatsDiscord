4cf0317bf067b4083e74e66bc91efade7494d982
P B <bogdan13531@gmail.com>
2023-05-06 19:49:50 +0600

    Added DiscordHostedClient

---
+++ DiscordHosted/Program.cs
@@ -0,0 +1,34 @@
+using Contracts;
+using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using Services;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureDiscordHost((context, config) =>
+    {
+        config.SocketConfig = new DiscordSocketConfig
+        {
+            LogLevel = LogSeverity.Verbose,
+            AlwaysDownloadUsers = true,
+            MessageCacheSize = 200
+        };
+        config.Token = Environment.GetEnvironmentVariable("StratzDiscordBotToken")!;
+    })
+    .UseInteractionService((context, config) =>
+    {
+        config.LogLevel = LogSeverity.Info;
+        config.UseCompiledLambda = true;
+    })
+    .ConfigureServices((context, services) =>
+    {
+        //Add any other services here
+        //services.AddHostedService<CommandHandler>();
+        //services.AddHostedService<InteractionHandler>();
+        //services.AddHostedService<BotStatusService>();
+        //services.AddHostedService<LongRunningService>();
+        services.AddScoped<IStratzClient, StratzClient>();
+    }).Build();
+
+await host.RunAsync();
