3f7896a342ffc55975a9db71888146a8c691b407
ddgbdn <51528928+ddgbdn@users.noreply.github.com>
2023-05-07 21:05:01 +0600

    Delete PostMatchStatsDiscord directory

--- PostMatchStatsDiscord/Constants/PlayerIds.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PostMatchStatsDiscord.Constants
-{
-    internal static class PlayerIds
-    {
-        public static long Okarin = 236888270;
-        public static long Lymior = 1273359352;
-        public static long MoJungle = 157966344;
-    }
-}
