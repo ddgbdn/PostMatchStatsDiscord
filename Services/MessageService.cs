4f28f498d97610eb1e1baaa5e5f1e041ee803c1c
P B <bogdan13531@gmail.com>
2023-05-10 15:18:08 +0600

    Resulting embed changed

--- Services/MessageService.cs
+++ Services/MessageService.cs
@@ -1,9 +1,11 @@
 ﻿using Contracts;
 using Discord;
+using Discord.Rest;
 using Discord.WebSocket;
 using Entities.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,18 +36,10 @@ namespace Services
 
             embed.Color = match.DidRadiantWin ? Color.Green : Color.Red;
 
-            var radiant = match.Players
-                .Where(p => p.IsRadiant)
-                .OrderBy(x => int.Parse(x.Position.Substring(x.Position.Length - 1, 1)))
-                .ToArray();
-            var dire = match.Players
-                .Where(p => !p.IsRadiant)
-                .OrderBy(x => int.Parse(x.Position.Substring(x.Position.Length - 1, 1)))
-                .ToArray();
+            var radiant = GetPlayersByTeam(match, true);
+            var dire = GetPlayersByTeam(match, false);
 
-            embed.AddField($"`                    {radiant.Select(x => x.Kills).Sum()}` - " + GetEmote("Radiant"), "‎ ", true);
-            embed.AddField($"` {match.DurationSeconds / 60}:{(match.DurationSeconds % 60).ToString().PadLeft(2, '0')} `", "‎ ", true);
-            embed.AddField(GetEmote("Dire") + " - " + $"`{dire.Select(x => x.Kills).Sum()}                     `", "‎ ", true);
+            AddHeaderFilds(embed, match, radiant, dire);
 
             for (int i = 0; i < radiant.Length; i++)
             {
@@ -58,17 +52,41 @@ namespace Services
             AddRewardField(embed, match, "TOP_CORE", 10);
             AddRewardField(embed, match, "TOP_SUPPORT", 13);
 
-            embed.Footer = new EmbedFooterBuilder().WithText("\npowered by Stratz");
+            AddFooterFields(embed, match);
 
             return embed.Build();
         }
 
+        private void AddHeaderFilds(EmbedBuilder embed, MatchStats match, Player[] radiant, Player[] dire)
+        {
+            embed.AddField(
+                match.DidRadiantWin ? "`WIN" : "`"
+                + radiant.Sum(p => p.Kills).ToString().PadLeft(match.DidRadiantWin ? 23 : 26) + "`"
+                + " - " + GetEmote("Radiant"),
+                "‎ ",
+                true);
+
+            embed.AddField($"` {TimeSpan.FromSeconds(match.DurationSeconds):mm\\:ss} `", "‎ ", true);
+
+            embed.AddField(
+                GetEmote("Dire") + " - "
+                + "`" + dire.Sum(p => p.Kills).ToString().PadRight(match.DidRadiantWin ? 26 : 23)
+                + (match.DidRadiantWin ? "`" : "WIN`"),
+                "‎ ",
+                true);
+        }
+
         private void AddStatsField(EmbedBuilder embed, Player player)
         {
+            var kills = player.Kills.ToString();
+            var deaths = player.Deaths.ToString();
+            var assists = player.Assists.ToString();
+
             embed.AddField(
                     GetEmote(GetRole(player.Position)) + " - "
                     + GetEmote(player.Hero.DisplayName)
-                    + $" {(player.SteamAccount.Name.Length > 15 ? player.SteamAccount.Name.Substring(15) : player.SteamAccount.Name)}",
+                    + $" {(player.SteamAccount.Name.Length > 11 ? player.SteamAccount.Name.Substring(0, 11) : player.SteamAccount.Name)}"
+                    + " - " + $"`{kills}/{deaths}/{assists}`",
                     GetStats(player), true);
         }
 
@@ -85,14 +103,42 @@ namespace Services
                 .WithIsInline(true);
         }
 
+        private void AddFooterFields(EmbedBuilder embed, MatchStats match)
+        {
+            embed.AddField("‎ ", "‎ ");
+
+            var mid = $"Mid: {GetLaneOutcome(match.MidLaneOutcome)}";
+            var top = $"Top: {GetLaneOutcome(match.TopLaneOutcome)}".PadRight((71 - mid.Length) / 2);
+            var bot = $"Bot: {GetLaneOutcome(match.BottomLaneOutcome)}".PadLeft((71 - mid.Length) / 2);
+
+            embed.AddField("` " + top + mid + bot + " `",
+                "` Powered by STRATZ" + $"id: {match.Id} `".PadLeft(56));
+        }
+
+        private Player[] GetPlayersByTeam(MatchStats match, bool isRadiant)
+            => match.Players
+                .Where(p => p.IsRadiant == isRadiant)
+                .OrderBy(x => int.Parse(x.Position.Substring(x.Position.Length - 1, 1)))
+                .ToArray();
+
         private string GetStats(Player player)
         {
-            var kills = player.Kills.ToString().PadLeft(2);
-            var deaths = player.Deaths.ToString().PadLeft(2);
-            var assists = player.Assists.ToString().PadLeft(2);
             var imp = player.Imp.ToString().PadLeft(3);
+            var heroDamage = player.HeroDamage / 1000.0;
+            var networth = player.Networth / 1000.0;
 
-            return $"`{kills}/{deaths}/{assists} imp:{imp} apm: {(int)player.Stats.ActionsPerMinute.Average()} `";
+            return $"` imp:{imp} hd: {heroDamage.ToString("F1", CultureInfo.InvariantCulture)}k netw: {networth:F1}k `";
+        }
+
+        private string GetLaneOutcome(string outcome)
+        {
+            if (outcome == "TIE")
+                return "Draw";
+
+            var words = outcome.Split('_')
+                .Select(w => w.Substring(0, 1) + w.Substring(1).ToLower());
+
+            return string.Join(" ", words);
         }
 
         private string GetRole(string pos)
