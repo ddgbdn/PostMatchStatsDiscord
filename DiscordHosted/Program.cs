d36608508d92cb4a37d0d9c0dbfbe2497be74117
P B <bogdan13531@gmail.com>
2023-05-29 16:31:41 +0600

    Add Serilog

--- DiscordHosted/Program.cs
+++ DiscordHosted/Program.cs
@@ -3,32 +3,61 @@ using Discord;
 using Discord.Addons.Hosting;
 using Discord.WebSocket;
 using DiscordHosted;
+using Serilog;
+using Serilog.Events;
 using Services;
 
-var host = Host.CreateDefaultBuilder(args)
-    .ConfigureDiscordHost((context, config) =>
-    {
-        config.SocketConfig = new DiscordSocketConfig
-        {
-            LogLevel = LogSeverity.Verbose,
-            AlwaysDownloadUsers = true,
-            MessageCacheSize = 200
-        };
-        config.Token = Environment.GetEnvironmentVariable("StratzDiscordBotToken")!;
-    })
-    .UseInteractionService((context, config) =>
-    {
-        config.LogLevel = LogSeverity.Info;
-        config.UseCompiledLambda = true;
-    })
-    .ConfigureServices((context, services) =>
-    {
-        services.AddHostedService<MatchProcessor>();
-        services.AddScoped<IStratzClient, StratzClient>();
-        services.AddScoped<IMessageService, MessageService>();
-        services.AddScoped<IMatchDataService, MatchIdDataService>();
-        services.AddScoped<ISubscriberDataService, SubscriberDataService>();
-        services.AddHostedService<InteractionHandler>();
-    }).Build();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+    .WriteTo.Console()
+    .WriteTo.File(".\\Logs\\log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
-await host.RunAsync();
+try
+{
+    Log.Information("Starting host");
+
+    var host = Host.CreateDefaultBuilder(args)
+        .UseSerilog()
+        .ConfigureDiscordHost((context, config) =>
+        {
+            config.SocketConfig = new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose,
+                AlwaysDownloadUsers = true,
+                MessageCacheSize = 200
+            };
+            config.Token = Environment.GetEnvironmentVariable("StratzDiscordBotToken")!;
+
+            config.LogFormat = (message, exception) => $"{message.Source}: {message.Message}";
+        })
+        .UseInteractionService((context, config) =>
+        {
+            config.LogLevel = LogSeverity.Info;
+            config.UseCompiledLambda = true;
+        })
+        .ConfigureServices((context, services) =>
+        {
+            services.AddHostedService<MatchProcessor>();
+            services.AddScoped<IStratzClient, StratzClient>();
+            services.AddScoped<IMessageService, MessageService>();
+            services.AddScoped<IMatchDataService, MatchIdDataService>();
+            services.AddScoped<ISubscriberDataService, SubscriberDataService>();
+
+            services.AddHostedService<InteractionHandler>();
+            services.AddHostedService<BotStatusService>();
+        }).Build();
+
+    await host.RunAsync();
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
---
+++ Services/BotStatusService.cs
@@ -0,0 +1,25 @@
+﻿using Discord.Addons.Hosting;
+using Discord.WebSocket;
+using Discord;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Addons.Hosting.Util;
+
+public class BotStatusService : DiscordClientService
+{
+    public BotStatusService(DiscordSocketClient client, ILogger<DiscordClientService> logger) : base(client, logger)
+    {
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await Client.WaitForReadyAsync(stoppingToken);
+        Logger.LogInformation("Client is ready!");
+
+        await Client.SetActivityAsync(new Game("/register id to start"));
+    }
+}
